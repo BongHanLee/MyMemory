bc03e732a9e9fea8a0f20cb392b50625e6628c74
BongHanLee <qhdgksl@gmail.com>
2018-09-16 02:14:30 +0900

    저장화면 구현

--- MyMemory/MemoFormVC.swift
+++ MyMemory/MemoFormVC.swift
@@ -17,6 +17,35 @@ class MemoFormVC: UIViewController, UIImagePickerControllerDelegate, UINavigatio
     
     //저장 버튼을 클릭했을 때 호출되는 메소드
     @IBAction func save(_ sender: Any) {
+        // 내용을 입력하지 않았을 경우, 경고한다.
+        guard self.contents.text?.isEmpty == false else {
+            let alert = UIAlertController(title: nil, message: "내용을 입력해주세요.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+            return
+        }
+        
+        //MemoData 객체를 생성하고, 데이터를 담는다.
+        let data = MemoData()
+        
+        data.Title = self.subject
+        data.contents = self.contents.text
+        data.image = self.preview.image
+        data.regdate = Date()
+        
+        //앱 델리게이트 객체를 일겅온 다음, memolist 배열에 MemoData 객체를 추가한다.
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.memolist.append(data)
+        
+        //작성폼 화면을 종료하고, 이전 화면으로 되돌아간다.
+        _ = self.navigationController?.popViewController(animated: true)
+        
+    }
+    
+    
+    
+    // 카메라 버튼을 클릭했을 때 호출되는 메소드
+    @IBAction func pick(_ sender: Any) {
         //이미지 피커 인스턴스를 생성한다.
         let picker = UIImagePickerController()
         
@@ -34,11 +63,6 @@ class MemoFormVC: UIViewController, UIImagePickerControllerDelegate, UINavigatio
         //이미지 피커 컨트롤러를 닫는다.
         picker.dismiss(animated: false)
     }
-    
-    // 카메라 버튼을 클릭했을 때 호출되는 메소드
-    @IBAction func pick(_ sender: Any) {
-    }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
